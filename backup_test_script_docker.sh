08a0c22e7e58e4d4253a75bceaf31932fb9603f5
Brandon Simmons <bsimmons@ycp.edu>
2023-01-12 14:46:10 -0500

    adding new script

---
+++ backup_test_script_docker.sh
@@ -0,0 +1,9 @@
+# (1) set up all the mysqldump variables
+DATE=`date +"%d_%b_%Y_%H%M"`
+SQLFILE=backup/db_backup_${DATE}.sql
+DATABASE=test
+USER=root
+DOCKER_CONTAINER_NAME=bsimmonsmysql
+
+# (3) do the mysql database backup (dump)
+docker exec -i ${DOCKER_CONTAINER_NAME} mysqldump --defaults-extra-file=home/config/mysqlpass.cnf -u ${USER} ${DATABASE} > ${SQLFILE}
