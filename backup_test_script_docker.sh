76917afa591d4fe7a4ecd39b546460f65dcfcbea
Brandon Simmons <bsimmons@ycp.edu>
2023-01-13 00:22:21 +0000

    adding dialogue to sh

--- backup_test_script_docker.sh
+++ backup_test_script_docker.sh
@@ -4,6 +4,11 @@ SQLFILE=backup/db_backup_${DATE}.sql
 DATABASE=test
 USER=root
 DOCKER_CONTAINER_NAME=bsimmonsmysql
+printf "Starting...\n"
+
+printf "Connecting to container\n"
 
 # (3) do the mysql database backup (dump)
 docker exec -i ${DOCKER_CONTAINER_NAME} mysqldump --defaults-extra-file=home/config/mysqlpass.cnf -u ${USER} ${DATABASE} > ${SQLFILE}
+
+printf "Database backup created at: ${SQLFILE}\n"
--- init_script.sh
+++ init_script.sh
@@ -3,10 +3,10 @@ printf "Running docker file\n"
 cd db_files
 docker build -t bsimmonsmysql:0.1 .
 
-printf "Running Container\n"
 cd ..
 
 printf "Checking backup dir\n"
 mkdir -p backup
 
+printf "Running Container\n"
 docker run --name=bsimmonsmysql --detach --publish 3306:3306 --volume=$(pwd)/backup:/home/backup --volume=$(pwd)/config:/home/config bsimmonsmysql:0.1
