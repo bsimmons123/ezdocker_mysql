67df9193c3a9297dcd6dfce1e13a0ba78cadcc32
Brandon Simmons <bsimmons@ycp.edu>
2023-01-12 23:42:50 -0500

    adding database connection into sql

--- backup_dev_script_docker.bat
+++ backup_dev_script_docker.bat
@@ -2,7 +2,7 @@
 ECHO off
 set time=%time:~0,2%_%time:~3,2%_%time:~6,2%
 set DIR=backup/
-set SQLFILE=db_backup_test_%time%.sql
+set SQLFILE=db_backup_dev_%time%.sql
 set SQLFILE_DIR=%DIR%%SQLFILE%
 set DATABASE=test
 set USER=root
@@ -16,18 +16,18 @@ docker exec -i %DOCKER_CONTAINER_NAME% mysqldump --defaults-extra-file=home/conf
 ECHO Database backup finished
 ECHO Find your backup file at: %SQLFILE_DIR%
 
-ECHO Adding (USE `test`) into %SQLFILE_DIR%
+ECHO Adding (USE `dev`) into %SQLFILE_DIR%
 
 cd backup
-echo USE `test`;>temp.txt
+echo USE `dev`;>temp.txt
 type %SQLFILE%>>temp.txt
 del %SQLFILE%
 rename temp.txt %SQLFILE%
 
-ECHO Adding (CREATE DATABASE  IF NOT EXISTS `test`) into %SQLFILE_DIR%
+ECHO Adding (CREATE DATABASE  IF NOT EXISTS `dev`) into %SQLFILE_DIR%
 
 
-echo CREATE DATABASE  IF NOT EXISTS `test`;>temp.txt
+echo CREATE DATABASE  IF NOT EXISTS `dev`;>temp.txt
 type %SQLFILE%>>temp.txt
 del %SQLFILE%
 rename temp.txt %SQLFILE%
